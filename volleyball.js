ea61740860896c5b3ba6adb9df0cd7b5878d7846
MarkIsNotTaken <mdemesillo@gmail.com>
2019-04-15 09:44:42 -1000

    fixing

--- volleyball.js
+++ volleyball.js
@@ -1,1 +1,22 @@
+function toggleSidebar(){
+  document.getElementById("sidebar").classList.toggle('active')
+}
+
+var myTimer;
+  function clock() {
+    myTimer = setInterval(myClock, 1000);
+    var c = 300;
+
+    function myClock() {
+      document.getElementById("time").innerHTML = --c;
+      if (c == 0) {
+        clearInterval(myTimer);
+        document.getElementById("time").innerHTML = "0"
+    }
+      var min = c / 60;
+      var sec = c % 60;
+      return min + " : " + sec;
+  }
+}
+
 
