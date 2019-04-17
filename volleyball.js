415ec8351eb69f68eba57aa5d20a375ab4b9d464
Jett M Palacpac <42420668+JettPack@users.noreply.github.com>
2019-04-17 10:01:28 -1000

    Add files via upload

--- volleyball.js
+++ volleyball.js
@@ -1,20 +1,7 @@
 function toggleSidebar(){
-  document.getElementById("sidebar").classList.toggle('active')
+	document.getElementById("sidebar").classList.toggle('active')
 }
 
-var myTimer;
-  function clock() {
-    myTimer = setInterval(myClock, 1000);
-    var c = 300;
-
-    function myClock() {
-      var min = Math.floor(c / 60);
-      var sec = c % 60;
-      document.getElementById("5").innerHTML = min + " : " + sec;
-      --c;
-      if (c == 0) {
-        clearInterval(myTimer);
-        document.getElementById("5").innerHTML = "0"
-    }
-  }
+function toggleDayone(){
+  document.getElementById("dayone").classList.toggle('active')
 }
